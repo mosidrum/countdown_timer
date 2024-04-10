7c4b31d7a49c010e1c198eb891d3801881f857e2
mosidrum <mosiokanga@gmail.com>
2024-04-10 12:40:55 +0100

    timmer stops

--- components/SelectedTime.js
+++ components/SelectedTime.js
@@ -4,24 +4,28 @@ import React, { useEffect, useState } from 'react';
 export default function SelectedTime(props) {
 	const { selectedTime, startCount } = props;
 	const [timeRemaining, setTimeRemaining] = useState(selectedTime * 60);
+	const [modal, setModal] = useState(false);
 
 	useEffect(() => {
 		let intervalId;
 
 		if (startCount && selectedTime > 0) {
 			intervalId = setInterval(() => {
-				setTimeRemaining((prevTime) => prevTime - 1);
+				setTimeRemaining((prevTime) => Math.max(0, prevTime - 1));
 			}, 1000);
+		} else {
+			clearInterval(intervalId);
 		}
+
 		return () => clearInterval(intervalId);
 	}, [startCount, selectedTime]);
 
 	const formatTime = (time) => {
 		const minutes = Math.floor(time / 60);
 		const seconds = time % 60;
-		return `${minutes.toString().padStart(2, '0')}mins : ${seconds
+		return `${minutes.toString().padStart(2, '0')}min : ${seconds
 			.toString()
-			.padStart(2, '0')}secs`;
+			.padStart(2, '0')}sec`;
 	};
 	return (
 		<View style={styles.container}>
